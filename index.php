85f493672ce0a5435f9b062f1cf790376a16bfb2
Sam <biryukwolf@yahoo.com>
2016-09-29 10:11:05 +0300

    add slider

--- index.php
+++ index.php
@@ -220,6 +220,9 @@
 
 
 
+
+
+
            
 
         </div>  </div>  
@@ -231,49 +234,60 @@
 
 <div class="container no-margin max_w">
   <div class="row">
-    <!-- Carousel -->
-<div id="myCarousel" class="carousel slide" data-ride="carousel">
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-    <li data-target="#myCarousel" data-slide-to="1"></li>
+    
 
-  </ol>
+                <div id="carousel-example-generic" class="carousel  slide" data-ride="carousel">
+                  <!-- Маркеры слайдов -->
+                  <ol class="carousel-indicators">
+                    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                  </ol>
+ 
+                  <!-- Содержимое слайдов -->
+                  <div class="carousel-inner"  role="listbox" >
+                    <div class="item active">
+                      <img src="images/slider1.png" alt="..." class="max_w" >
+                      <div class="carousel-caption">
+                        <h3>Россия (Москва)</h3>
+                        <p>Где это видано, чтобы люди в Москве без прописки проживали.</p>
+                      </div>
+                    </div>
+                     
+                   <div class="item">
+                      <img src="images/slider2.png" alt="..." class="max_w" >
+                      <div class="carousel-caption">
+                        <h3>Осень</h3>
+                        <p>Осень — это вторая весна, когда каждый лист — цветок.</p>
+                      </div>
+                    </div>
+                     
+                    <div class="item">
+                      <img src="images/slider3.png" alt="..." class="max_w">
+                      <div class="carousel-caption">
+                        <h3>Дождь</h3>
+                        <p>Осень опять идут дожди...</p>
+                      </div>
+                    </div> 
+                  </div>
+ 
+                  <!-- Controls -->
+                  <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left"></span>
+                  </a>
+                  <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right"></span>
+                  </a>
+                </div>
+ 
+            
 
-  <!-- Wrapper for slides -->
-  <div class="carousel-inner" role="listbox">
-    <div class="item active">
-      <img src="images/slider1.png" alt="Chania" class="" >
-      <div class="carousel-caption">
-        <h3>Chania</h3>
-        <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
-      </div>
-    </div>
-
-    <div class="item">
-      <img src="images/slider1.png" alt="Chania"  class="">
-      <div class="carousel-caption">
-        <h3>Chania</h3>
-        <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
-      </div>
-    </div>
 
 
 
-  <!-- Left and right controls -->
-  <a  class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a  class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
 
-     <!-- end Carusele -->
+
   </div>
-</div>
 <!-- end wrap carusele -->
 
 
